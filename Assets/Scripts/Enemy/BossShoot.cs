d906966f85171b8170625369dea191ec2e663f2f
Nathalia <47846610+nathaliandrad@users.noreply.github.com>
2023-10-04 08:20:24 +0000

    Update BossShoot.cs

--- Assets/Scripts/Enemy/BossShoot.cs
+++ Assets/Scripts/Enemy/BossShoot.cs
@@ -1,60 +1,43 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BossShoot : MonoBehaviour
 {
-    float timer;
-    float secondTimer;
     public GameObject bossBullet;
     public GameObject smallBossBullet;
-    public GameObject nozzle;
-    public GameObject smallNozzle;
-    public GameObject smallNozzle2;
-    public GameObject smallNozzle3;
-    public GameObject smallNozzle4;
+    public GameObject[] nozzles;
+    public GameObject[] smallNozzles;
+    private float timer = 0f;
+    private float secondTimer = 0f;
 
-    public float distanceToShoot;
+    public float shootingInterval = 18f;
+    public float smallShootingInterval = 15f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-       
-        if (timer == 0)
+        timer += Time.deltaTime;
+        secondTimer += Time.deltaTime;
+
+        if (timer >= shootingInterval)
         {
-            Shoot();
-        }
-        timer += 0.2f;
-        if (timer > 18)
-        {
-            timer = 0;
+            Shoot(bossBullet, nozzles);
+            timer = 0f;
         }
 
-        if (secondTimer == 0)
+        if (secondTimer >= smallShootingInterval)
         {
-            SmallBossShoot();
+            Shoot(smallBossBullet, smallNozzles);
+            secondTimer = 0f;
         }
-        secondTimer += 0.5f;
-        if (secondTimer > 15)
-        {
-            secondTimer = 0;
-        }
-
-
     }
 
-    public void Shoot()
+    private void Shoot(GameObject bulletPrefab, GameObject[] shootPoints)
     {
-        Instantiate(bossBullet, nozzle.transform.position, nozzle.transform.rotation);
-       // Instantiate(bossBullet, nozzleRight.transform.position, nozzleRight.transform.rotation);
-    }
-
-    public void SmallBossShoot()
-    {
-        Instantiate(smallBossBullet, smallNozzle.transform.position, smallNozzle.transform.rotation);
-        Instantiate(smallBossBullet, smallNozzle2.transform.position, smallNozzle2.transform.rotation);
-        Instantiate(smallBossBullet, smallNozzle3.transform.position, smallNozzle3.transform.rotation);
-        Instantiate(smallBossBullet, smallNozzle4.transform.position, smallNozzle4.transform.rotation);
-
+        foreach (var nozzle in shootPoints)
+        {
+            Instantiate(bulletPrefab, nozzle.transform.position, nozzle.transform.rotation);
+        }
     }
 }
