bef20da32dc71ba4835bd4df61ee47fe7b1abe9b
Nathalia <47846610+nathaliandrad@users.noreply.github.com>
2023-10-04 08:21:43 +0000

    Update Enemy2.cs

--- Assets/Scripts/Enemy/Enemy2.cs
+++ Assets/Scripts/Enemy/Enemy2.cs
@@ -1,71 +1,69 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy2 : MonoBehaviour
 {
-    public int enemy2Health;
-    public float randomEnemySpeedFaster;
-
+    public int enemy2Health = 5;
+    public float randomEnemySpeedFaster = Random.Range(20f, 40f);
     public AudioSource speaker;
     public AudioClip enem2Hit;
-
     public GameObject hitEfx;
 
     // Start is called before the first frame update
     void Start()
     {
-        randomEnemySpeedFaster = Random.Range(20f, 40f);
-        enemy2Health = 5;
         Destroy(gameObject, 30);
-        //transform.Rotate(0.0f, 180.0f, 0.0f);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(new Vector3(0, 0, -randomEnemySpeedFaster) * Time.deltaTime);
-
+        MoveEnemy();
     }
+
+    void MoveEnemy()
+    {
+        transform.Translate(Vector3.back * randomEnemySpeedFaster * Time.deltaTime);
+    }
+
     void OnTriggerEnter(Collider other)
     {
+        HandleCollision(other);
+    }
 
-
+    void HandleCollision(Collider other)
+    {
         Physics.IgnoreLayerCollision(8, 9);
 
         if (other.CompareTag("Bullet"))
         {
-            speaker.PlayOneShot(enem2Hit, 1);
-            Destroy(other.gameObject);
-            GM.score += 50;
-            enemy2Health -= 1;
-            if (enemy2Health < 0)
-            {
-                speaker.PlayOneShot(enem2Hit, 1);
-                Instantiate(hitEfx, transform.position, transform.rotation);
-                Destroy(gameObject,0.1f);
-                enemy2Health = 5;
-            }
+            HandleBulletHit(1, 50);
         }
-
-        if (other.CompareTag("PowerBullet"))
+        else if (other.CompareTag("PowerBullet"))
         {
-            speaker.PlayOneShot(enem2Hit, 1);
-            Destroy(other.gameObject);
-            GM.score += 200;
-            enemy2Health -= 4;
-            if (enemy2Health < 0)
-            {
-                speaker.PlayOneShot(enem2Hit, 1);
-                Instantiate(hitEfx, transform.position, transform.rotation);
-                Destroy(gameObject,0.1f);
-                enemy2Health = 5;
-            }
+            HandleBulletHit(4, 200);
         }
-
-
-
     }
 
+    void HandleBulletHit(int damage, int scoreIncrease)
+    {
+        speaker.PlayOneShot(enem2Hit, 1);
+        Destroy(other.gameObject);
+        GM.score += scoreIncrease;
+        enemy2Health -= damage;
 
+        if (enemy2Health < 0)
+        {
+            DestroyEnemy();
+        }
+    }
+
+    void DestroyEnemy()
+    {
+        speaker.PlayOneShot(enem2Hit, 1);
+        Instantiate(hitEfx, transform.position, transform.rotation);
+        Destroy(gameObject, 0.1f);
+        enemy2Health = 5;
+    }
 }
