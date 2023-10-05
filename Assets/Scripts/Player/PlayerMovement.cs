269ff7810e91507cdb1fa292e430d7adc6179028
Nathalia <47846610+nathaliandrad@users.noreply.github.com>
2023-10-05 08:11:05 +0000

    Update PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,153 +1,130 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Settings")]
+    [SerializeField] private float movementSpeed = 20f;
+    [SerializeField] private float rotationPlane = 15.0f;
+    [SerializeField] private float maxLimitLeft = -25f;
+    [SerializeField] private float maxLimitRight = 25;
+    [SerializeField] private float maxLimitDown = -25f;
+    [SerializeField] private float maxLimitTop = 25f;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float speedY = 0.9f;
+    [SerializeField] private float turbineRot = 1f;
 
-    float movementSpeed = 20f;
-    float rotationPlane = 15.0f;
-    public float angles;
-    public float anglesXX;
+    [Header("Game Objects")]
+    [SerializeField] private GameObject leftWall;
+    [SerializeField] private GameObject rightWall;
+    [SerializeField] private GameObject cam;
+    [SerializeField] private Transform turbineLeft;
+    [SerializeField] private Transform turbineRight;
 
-    float maxLimitLeft = -25f;
-    float maxLimitRight = 25;
-    float maxLimitDown = -25f;
-    float maxLimitTop = 25f;
-
-    public bool movingHoriLeft = false;
-    public bool movingHoriRight = false;
-
-    public float speed;
-    public float speedY;
-
-    public GameObject leftWall, rightWall;
-    public GameObject cam;
-
-    public Transform turbineLeft;
-    public Transform turbineRight;
-
-    public float turbineRot;
-
-   // public float angles;
-    private Vector3 offset;
-
-
-    void Start()
+    private float angles;
+    private float anglesXX;
+    
+    private void Start()
     {
-        turbineRot = 1f;
         anglesXX = 0;
-        speed = 1f;
-        speedY = 0.9f;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-       transform.Translate(new Vector3(0, 0, movementSpeed) * Time.deltaTime);
-        //moving left
+        MoveForward();
+        Rotate();
+    }
+
+    private void MoveForward()
+    {
+        transform.Translate(new Vector3(0, 0, movementSpeed) * Time.deltaTime);
+    }
+
+    private void Rotate()
+    {
+        // Rotation for turning left
         if (Input.GetKey(KeyCode.A) && (transform.position.x > maxLimitLeft))
         {
-            turbineLeft.transform.Rotate(new Vector3(0, -turbineRot, 0),Space.Self);
-            turbineRight.transform.Rotate(new Vector3(0, -turbineRot, 0), Space.Self);
-
-            transform.Translate(new Vector3(-speed, 0, 0), Space.World);
-           
-            cam.transform.Translate(new Vector3(-speed / 2, 0, 0), Space.World);
             RotateLeft();
-            
         }
-        //moving right
-        if (Input.GetKey(KeyCode.D) && (transform.position.x < maxLimitRight))
+        // Rotation for turning right
+        else if (Input.GetKey(KeyCode.D) && (transform.position.x < maxLimitRight))
         {
-            turbineLeft.transform.Rotate(new Vector3(0, turbineRot, 0), Space.Self);
-            turbineRight.transform.Rotate(new Vector3(0, turbineRot, 0), Space.Self);
-            transform.Translate(new Vector3(speed, 0, 0), Space.World);
-            cam.transform.Translate(new Vector3(speed / 2, 0, 0), Space.World);
             RotateRight();
         }
-        //checking rotation
-        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        // Reset rotation when not turning
+        else
         {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            turbineLeft.transform.Rotate(new Vector3(0, 0, 0), Space.Self);
-            turbineRight.transform.Rotate(new Vector3(0, 0, 0), Space.Self);
-
-        }
-        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
-        {
-            //  transform.eulerAngles = new Vector3(0, 0, 0);
-            turbineLeft.transform.Rotate(new Vector3(0, 0, 0), Space.Self);
-            turbineRight.transform.Rotate(new Vector3(0, 0, 0), Space.Self);
-
+            ResetRotation();
         }
 
-        //moving up
+        // Rotation for moving up
         if (Input.GetKey(KeyCode.W) && (transform.position.y < maxLimitTop))
         {
-
-            turbineLeft.transform.Rotate(new Vector3(-turbineRot, 0, 0), Space.Self);
-            turbineRight.transform.Rotate(new Vector3(-turbineRot, 0, 0), Space.Self);
-            transform.Translate(new Vector3(0, speedY, 0), Space.World);
-            cam.transform.Translate(new Vector3(0, speed / 1.5f, 0), Space.World);
             RotateUp();
-        }//moving down
-        if (Input.GetKey(KeyCode.S) && (transform.position.y > maxLimitDown))
+        }
+        // Rotation for moving down
+        else if (Input.GetKey(KeyCode.S) && (transform.position.y > maxLimitDown))
         {
-
-            turbineLeft.transform.Rotate(new Vector3(turbineRot, 0, 0), Space.Self);
-            turbineRight.transform.Rotate(new Vector3(turbineRot, 0, 0), Space.Self);
-            // transform.eulerAngles = new Vector3(anglesXX, 0, 0);
-            transform.Translate(new Vector3(0, -speedY, 0) , Space.World);
-            cam.transform.Translate(new Vector3(0, -speed / 1.5f, 0), Space.World);
             RotateDown();
         }
-
+        // Reset vertical rotation when not moving up/down
+        else
+        {
+            ResetVerticalRotation();
+        }
     }
 
-
-    void RotateUp()
+    private void RotateLeft()
     {
+        turbineLeft.Rotate(new Vector3(0, -turbineRot, 0), Space.Self);
+        turbineRight.Rotate(new Vector3(0, -turbineRot, 0), Space.Self);
+        transform.Translate(new Vector3(-speed, 0, 0), Space.World);
+        cam.transform.Translate(new Vector3(-speed / 2, 0, 0), Space.World);
+        if (angles < 0) angles = 0;
+        if (angles < 30) angles += 10f;
+        transform.eulerAngles = new Vector3(0, 0, angles);
+    }
 
+    private void RotateRight()
+    {
+        turbineLeft.Rotate(new Vector3(0, turbineRot, 0), Space.Self);
+        turbineRight.Rotate(new Vector3(0, turbineRot, 0), Space.Self);
+        transform.Translate(new Vector3(speed, 0, 0), Space.World);
+        cam.transform.Translate(new Vector3(speed / 2, 0, 0), Space.World);
+        if (angles > 0) angles = 0;
+        if (angles > -30) angles -= 10f;
+        transform.eulerAngles = new Vector3(0, 0, angles);
+    }
+
+    private void ResetRotation()
+    {
+        transform.eulerAngles = Vector3.zero;
+        turbineLeft.localRotation = Quaternion.identity;
+        turbineRight.localRotation = Quaternion.identity;
+    }
+
+    private void RotateUp()
+    {
+        turbineLeft.Rotate(new Vector3(-turbineRot, 0, 0), Space.Self);
+        turbineRight.Rotate(new Vector3(-turbineRot, 0, 0), Space.Self);
+        transform.Translate(new Vector3(0, speedY, 0), Space.World);
+        cam.transform.Translate(new Vector3(0, speed / 1.5f, 0), Space.World);
         transform.eulerAngles = new Vector3(-rotationPlane, 0, 0);
     }
-    void RotateDown()
-    {
 
+    private void RotateDown()
+    {
+        turbineLeft.Rotate(new Vector3(turbineRot, 0, 0), Space.Self);
+        turbineRight.Rotate(new Vector3(turbineRot, 0, 0), Space.Self);
+        transform.Translate(new Vector3(0, -speedY, 0), Space.World);
+        cam.transform.Translate(new Vector3(0, -speed / 1.5f, 0), Space.World);
         transform.eulerAngles = new Vector3(rotationPlane, 0, 0);
     }
-    void RotateLeft()
+
+    private void ResetVerticalRotation()
     {
-        if (angles < 0) //if my angle is facing right - pob back to 0
-        {
-            angles = 0;
-        }
-        if (angles < 30) //limit my angles to 25f
-        {
-            angles += 10f;
-        }
-
-        transform.eulerAngles = new Vector3(0, 0, angles);
-    }
-
-    void RotateLeftUp()
-    {
-        transform.eulerAngles = new Vector3(angles, 0, angles);
-    }
-
-
-
-    void RotateRight()
-    {
-        if (angles > 0)
-        {
-            angles = 0;
-        }
-        if (angles > -30)
-        {
-            angles -= 10f;
-        }
-
-        transform.eulerAngles = new Vector3(0, 0, angles);
+        turbineLeft.localRotation = Quaternion.identity;
+        turbineRight.localRotation = Quaternion.identity;
     }
 }
